98135034f18b55d8faaa05befc3d71fda5bc35e5
ToavinaMendrika <mendrika.toavina.andriantsivolosy@esti.mg>
2019-07-19 23:02:49 +0300

    add postcss auto-prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ const {src, dest, series, parallel, watch} = require('gulp')
 const gulpSass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
 const del = require('del')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+
+var postcssPlugins = [
+    autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')
+];
 
 function clean(){
     return del('dist')
@@ -10,6 +16,7 @@ function clean(){
 function scss(){
     return src('./src/scss/**/*.scss',{ sourcemaps: true })
         .pipe(gulpSass())
+        .pipe(postcss(postcssPlugins))
         .pipe(sourcemaps.write('.'))
         .pipe(dest('./dist/css'),{ sourcemaps: '.' })
 }
